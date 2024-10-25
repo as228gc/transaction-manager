46d82ec999ea3e3d0686a3aadb342142721a9728
Alex Söderberg <as228gc@student.lnu.se>
2024-10-25 11:35:30 +0200

    Add display of report

--- src/views/Report/ReportView.tsx
+++ src/views/Report/ReportView.tsx
@@ -1,15 +1,49 @@
-import { Report } from "tra-ma";
+import { Report } from "tra-ma"
+import "./ReportView.css"
 
 interface ReportViewProps {
   report: Report
 }
 
-export const ReportView: React.FC<ReportViewProps> = ({report}) => {
-  return(
-    <div>
-      <p>
-        {report.toString()}
-      </p>
+export const ReportView: React.FC<ReportViewProps> = ({ report }) => {
+  console.log(report.getExpensesByCategory())
+
+  return (
+    <div className="bordered content-start m-2 p-2">
+      <h2>Start date: </h2>
+      <p>{report.getStartDate().toISOString().slice(0,10)}</p>
+      <h2>End date</h2>
+      <p>{report.getEndDate().toISOString().slice(0, 10)}</p>
+      <h2>Total income</h2>
+      <p>{report.getTotalIncome()}</p>
+      <h2>Total expenses</h2>
+      <p>{report.getTotalExpenses()}</p>
+
+      <h2>Expenses by category</h2>
+      <ul>
+        {
+          report.getExpensesByCategory().map((item) => {
+            return (
+              <li className="content-start">
+                <p>{item['Category']}: {item['TotalAmount']}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
+
+      <h2>Income by category</h2>
+      <ul>
+        {
+          report.getIncomeByCategory().map((item) => {
+            return (
+              <li className="content-start">
+                <p>{item['Category']}: {item['TotalAmount']}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
